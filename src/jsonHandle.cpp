8e127a85a714ced8a22edf0f14ca43f0b6099f20
Oasis <shengoasis@gmail.com>
2025-03-14 14:09:36 +0800

    添加jsonHandle到git

---
+++ src/jsonHandle.cpp
@@ -0,0 +1,17 @@
+#include "jsonHandle.h"
+
+// 解析 JSON 输入（C++ server这边不需要判断数据的合法性，只需要接收数据然后处理就行）
+RequestParameters parseInput(const json& j) {
+    return {
+        j.value("stream_path", ""),
+        j.value("query_path", ""),
+        j.value("result_mode", ""),
+        j.value("execute_mode", ""),
+        j.value("data_percent", ""),
+        j.value("index_time_limit", ""),
+        j.value("online_time_limit", ""),
+        j.value("is_using_static_merge", ""),
+        j.value("is_using_dynamic_merge", ""),
+        j.value("is_return_match_result", "")
+    };
+}
